38d90606cf403e16ba4eeace61d3b724b900c22d
Wiki-Episteme <118081759+Wiki-EpisTeme@users.noreply.github.com>
2024-07-29 00:01:01 -0500

    solo un hola

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import "./style.css";
 import { setupCounter } from "./counter.js";
 
 document.querySelector("#app").innerHTML = `
-  <input type="password" class="form-control" id="exampleInputPassword1">
+<h1>Hola......</h1>
 `;
 
 setupCounter(document.querySelector("#counter"));
